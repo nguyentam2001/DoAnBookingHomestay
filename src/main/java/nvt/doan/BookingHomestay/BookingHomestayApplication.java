8e69df2d415e44078026b4e10ec44bf7190d6191
TamNguyen <2001tambh@gmail.com>
2023-03-18 16:44:28 +0700

    setup-project

---
+++ src/main/java/nvt/doan/BookingHomestay/BookingHomestayApplication.java
@@ -0,0 +1,13 @@
+package nvt.doan.BookingHomestay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingHomestayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingHomestayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nvt/doan/BookingHomestay/BookingHomestayApplicationTests.java
@@ -0,0 +1,13 @@
+package nvt.doan.BookingHomestay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingHomestayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
